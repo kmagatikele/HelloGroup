511fc2bebe4f16e23ea87e09bb7a469fc6bb298b
Keletso Moselakgomo <Keletso.Moselakgomo@mixtelematics.com>
2024-05-02 16:41:09 +0200

    Create database

---
+++ HelloGroup.Repository/Database/Script.sql
@@ -0,0 +1,12 @@
+﻿CREATE DATABASE HelloGroup;
+USE HelloGroup;
+CREATE TABLE [Transactions] (
+    TransactionId BIGINT,
+    LineNumber BIGINT NULL,
+    FCDebit FLOAT NULL,
+	FCCredit FLOAT NULL,
+	Debit FLOAT NULL,
+	Credit FLOAT NULL,
+	PostDate DATE NULL,
+	Currency VARCHAR(50) NULL
+);
